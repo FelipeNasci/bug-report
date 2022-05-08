93d2f975fe15b2ae5045d86ed29c1617e469b60e
FelipeNasci <dfnascimento@live.com>
2022-05-08 10:11:18 +0000

    feat(comp): use widget-form

--- src/components/widgets/Widgets.tsx
+++ src/components/widgets/Widgets.tsx
@@ -1,10 +1,11 @@
 import { ChatTeardropDots } from "phosphor-react";
 import { Popover } from "@headlessui/react";
+import { WidgetForm } from "../WidgetForm";
 
 export const Widgets = () => (
-  <Popover className="absolute bottom-4 right-5">
+  <Popover className="absolute bottom-4 right-5 md:bottom-8 md:right-8 flex flex-col items-end">
     <Popover.Panel>
-      <p>Hello World</p>
+      <WidgetForm />
     </Popover.Panel>
     <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
       <ChatTeardropDots className="w-6 h6" />
