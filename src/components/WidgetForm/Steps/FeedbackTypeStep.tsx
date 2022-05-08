3316cbcebfd22bbe70fda59a6d86ea4defdeec75
FelipeNasci <dfnascimento@live.com>
2022-05-08 10:27:14 +0000

    refactor(widget-form): change style

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -8,7 +8,7 @@ interface FeedbackTypeStepProps {
 export const FeedbackTypeStep = ({
   setFeedbackType,
 }: FeedbackTypeStepProps) => (
-  <div className="flex py-0 gap-2 w-full">
+  <div className="flex py-0 my-2 gap-2 w-full">
     {Object.entries(feedbackTypes).map(([key, value]) => (
       <button
         className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
--- src/components/WidgetForm/WidgetForm.tsx
+++ src/components/WidgetForm/WidgetForm.tsx
@@ -8,7 +8,7 @@ export const WidgetForm = () => {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
   return (
-    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-2 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-10">Deixe seu feedback</span>
         <CloseButton />
