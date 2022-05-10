1591bd3c5f0ccd872007c23054642c551c62081f
FelipeNasci <dfnascimento@live.com>
2022-05-10 00:44:38 +0000

    feat: finish component

--- src/components/WidgetForm/WidgetForm.tsx
+++ src/components/WidgetForm/WidgetForm.tsx
@@ -1,26 +1,41 @@
 import { useState } from "react";
-import { CloseButton } from "../CloseButton";
 
-import { FeedbackTypeStep } from "./Steps";
+import {
+  FeedbackTypeStep,
+  FeedbackContentStep,
+  FeedbackSuccessStep,
+} from "./Steps";
 import { FeedbackType } from "./types";
 
 export const WidgetForm = () => {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSended, setFeedbackSended] = useState(false);
+
+  function handleFeedbackReset() {
+    setFeedbackType(null);
+    setFeedbackSended(false);
+  }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-2 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-10">Deixe seu feedback</span>
-        <CloseButton />
-      </header>
-      {feedbackType ? (
-        <span>{feedbackType}</span>
+      {feedbackSended ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleFeedbackReset} />
       ) : (
-        <FeedbackTypeStep setFeedbackType={setFeedbackType} />
+        <>
+          {feedbackType ? (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackResetRequest={handleFeedbackReset}
+              onSendFeedback={setFeedbackSended}
+            />
+          ) : (
+            <FeedbackTypeStep setFeedbackType={setFeedbackType} />
+          )}
+        </>
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ por{" "}
+        Done with ♥ by{" "}
         <a
           className="underline underline-offset-2"
           href="https://www.github.com/felipenasci"
