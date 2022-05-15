47773db6d1a6a363ccfc7f713dc3449e0363acf7
FelipeNasci <dfnascimento@live.com>
2022-05-15 18:51:24 +0000

    refactor: move components to correct folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { Widgets } from "./base-components";
+import { Widgets } from "./Widgets";
 
 function App() {
   return <Widgets />;
--- src/base-components/Loading/Loading.tsx
+++
@@ -1,7 +0,0 @@
-import { CircleNotch } from "../icons";
-
-export const Loading = () => (
-  <div className="w-6 h-6 flex items-center justify-center overflow-hidden">
-    <CircleNotch className="w-6 h-6 animate-spin" />
-  </div>
-);
