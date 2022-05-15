40dea840678669d23ca45b5573249481f5dc279f
FelipeNasci <dfnascimento@live.com>
2022-05-15 19:01:37 +0000

    refactor: remove unused files

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-
-import { Widgets } from "./Widgets";
-
-function App() {
-  return <Widgets />;
-}
-
-export default App;
--- src/stories/Widget.stories.tsx
+++ src/stories/Widget.stories.tsx
@@ -1,6 +1,6 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { Widgets } from "..";
+import { Widgets } from "../Widgets";
 
 export default {
   title: "App/Widgets",
